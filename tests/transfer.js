61d1ec105919b67a9748f36adbd2b2d7c6ec5734
Andrew Mikhailov <andrew@vitche.com>
2019-04-08 14:04:36 +0000

    1. Added unit tests for the ERC20.trasfer method.

---
+++ tests/transfer.js
@@ -0,0 +1,24 @@
+var web3 = require('web3');
+var web3Utils = require('web3-utils');
+var baseTest = require('../base-test');
+var compare = require('../compare');
+module.exports = baseTest;
+baseTest.testTransfer = function (test) {
+    var connection = baseTest.helpers.getConnection();
+    var contractInstance = baseTest.helpers.getContractInstance();
+    var sender = baseTest.owner;
+    var firstAccount = baseTest.accounts[0].address;
+    var amount = web3Utils.toWei("1", "ether");
+    var transactionRequest = {
+        from: sender/*,
+        value: amount*/
+    };
+    connection.eth.estimateGas(transactionRequest, function (error, gasEstimate) {
+        transactionRequest.gas = 10 * gasEstimate;
+        contractInstance.methods.transfer(firstAccount, amount).send(transactionRequest)
+            .on('confirmation', function (confirmationNumber, receipt) {
+                console.log('Transaction confirmation:', confirmationNumber, receipt);
+                test.done();
+            });
+    });
+}
