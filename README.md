603661d92533f0099666f4b73a1c631f7776eb37
Vitche Research Team <developer@vitche.com>
2020-09-11 21:42:30 -0400

    1. Fixed starting the test network. 2. More description on installing.

--- README.md
+++ README.md
@@ -6,10 +6,20 @@
 You need to install Golang first.
 
 ### Install Geth on Debian
+To build Geth:
 ```shell
 export GO111MODULE=on
 go get -d github.com/ethereum/go-ethereum@v1.9.9
 go install github.com/ethereum/go-ethereum/cmd/geth
+```
+To install under the user:
+
+```shell
+mv $GOPATH/bin/geth /usr/local/bin
+```
+To install under root:
+```shell
+mv /root/go/bin/geth /usr/local/bin
 ```
 
 ## Starting the test network
--- test-network.sh
+++ test-network.sh
@@ -1,1 +1,2 @@
-geth --dev --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpcapi="db,eth,net,web3,personal,miner" --datadir /tmp/.ethereum/development --ipcpath $HOME/.ethereum/geth.ipc console
+# TODO: The following module is no more available: db
+geth --dev --allow-insecure-unlock --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpcapi="eth,net,web3,personal,miner" --datadir /tmp/.ethereum/development --ipcpath $HOME/.ethereum/geth.ipc console
