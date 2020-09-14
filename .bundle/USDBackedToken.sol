a7eb2974c2a6989d78dccfc361790a5f46af465c
Vitche Research Team Developer <developer@vitche.com>
2020-09-14 19:47:32 +0300

    1.  Метод обновления баланса пользователя по курсу

--- .bundle/USDBackedToken.sol
+++ .bundle/USDBackedToken.sol
@@ -1,16 +1,27 @@
-pragma solidity ^0.6.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.7.0;
 
-import "ERC20.sol";
+import "./ERC20.sol";
 
 contract USDBackedToken is ERC20 {
 
     uint8 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 1000000 * (10 ** uint256(DECIMALS));
 
-    constructor () public ERC20("USDBackedToken", "USDBT") {
+    /**
+     * The starting Bitcoin to USD exchange rate to perform supply adjustments.
+     */
+    uint8 public previousBTCUSDRate = 10000;
+
+    constructor () public ERC20("SimpleToken", "SIM") {
     }
 
     function mint(address account, uint256 amount) public {
         _mint(account, amount);
+    }
+
+    function adjust(address account, uint8 currentBTCUSDRate) {
+        uint8 percentage = previousBTCUSDRate * 100 / currentBTCUSDRate;
+        _balances[account] = _balances[account] * percentage / 100;
     }
 }
