653895de75ee03dc272680a0f8d9e31c0dd3b2fa
Vitche Research Team <developer@vitche.com>
2020-09-17 22:57:20 -0400

    1. Исправления математики. 2. Экономия ресурсов за счет отсутствия повторных расчетов.

--- contracts/examples/DFY.sol
+++ contracts/examples/DFY.sol
@@ -19,17 +19,23 @@ contract DFY is USDBackedBTCToken {
         mint(0x37DB0F7ddC14baf6FD9f0484E9E63B658419aF53, singleAmount);
     }
 
-    function adjust(uint16 currentBTCUSDRate) public {
+    function adjust(uint256 currentBTCUSDRate) public {
 
-        adjust(0x9A54387CD45d73b760d20B305CF2f9c664F25DF3, currentBTCUSDRate);
-        adjust(0x5bc8C17A52885Fc10e6e6C1742DA1E064eC8b8E9, currentBTCUSDRate);
-        adjust(0xEe4d51f6d3Da5417D74Ca6DaAB07490CB76d0e8d, currentBTCUSDRate);
-        adjust(0x8be903A229bBB069E7059c7838B30e677fBC4C30, currentBTCUSDRate);
-        adjust(0x0230699450099B80631b49672153684eE31af0Fd, currentBTCUSDRate);
-        adjust(0xb501dEF0915b2c1359058d97234F95894a8eA647, currentBTCUSDRate);
-        adjust(0xf99CC2aeFa3bD4FDfAcC43d4086788f10ee06B55, currentBTCUSDRate);
-        adjust(0xE6613282a79c691c9fcC9D5Cb460486D3DA2eaB8, currentBTCUSDRate);
-        adjust(0xD5abAaB14e81B8AE57D2Bee0024E1c593BB5A1D0, currentBTCUSDRate);
-        adjust(0x37DB0F7ddC14baf6FD9f0484E9E63B658419aF53, currentBTCUSDRate);
+        uint256 percentage = calculatePercentage(currentBTCUSDRate);
+
+        adjust(0x9A54387CD45d73b760d20B305CF2f9c664F25DF3, percentage);
+        adjust(0x5bc8C17A52885Fc10e6e6C1742DA1E064eC8b8E9, percentage);
+        adjust(0xEe4d51f6d3Da5417D74Ca6DaAB07490CB76d0e8d, percentage);
+        adjust(0x8be903A229bBB069E7059c7838B30e677fBC4C30, percentage);
+        adjust(0x0230699450099B80631b49672153684eE31af0Fd, percentage);
+        adjust(0xb501dEF0915b2c1359058d97234F95894a8eA647, percentage);
+        adjust(0xf99CC2aeFa3bD4FDfAcC43d4086788f10ee06B55, percentage);
+        adjust(0xE6613282a79c691c9fcC9D5Cb460486D3DA2eaB8, percentage);
+        adjust(0xD5abAaB14e81B8AE57D2Bee0024E1c593BB5A1D0, percentage);
+        adjust(0x37DB0F7ddC14baf6FD9f0484E9E63B658419aF53, percentage);
+
+        adjustTotalSupply(percentage);
+
+        previousBTCUSDRate = currentBTCUSDRate;
     }
 }
--- tests/dfy.js
+++ tests/dfy.js
@@ -1,11 +1,11 @@
 const web3Utils = require('web3-utils');
 const baseTest = require('../base-test');
 module.exports = baseTest;
-/*
+const firstInvestor = "0x9A54387CD45d73b760d20B305CF2f9c664F25DF3";
+const secondInvestor = "0x5bc8C17A52885Fc10e6e6C1742DA1E064eC8b8E9";
 baseTest.testInitialBalance = async function (test) {
     test.expect(1);
     const contractInstance = baseTest.helpers.getContractInstance();
-    const firstInvestor = "0x9A54387CD45d73b760d20B305CF2f9c664F25DF3";
     const amount = web3Utils.toWei("100000", "ether");
     contractInstance.methods.balanceOf(firstInvestor).call().then(function (balance) {
         console.log("Investor balance", balance);
@@ -18,7 +18,6 @@ baseTest.testSingularAdjust = async function (test) {
     const connection = baseTest.helpers.getConnection();
     const contractInstance = baseTest.helpers.getContractInstance();
     const sender = baseTest.owner;
-    const firstInvestor = "0x9A54387CD45d73b760d20B305CF2f9c664F25DF3";
     const amount = web3Utils.toWei("100000", "ether");
     const transactionRequest = {
         from: sender
@@ -36,6 +35,67 @@ baseTest.testSingularAdjust = async function (test) {
                 test.ok(parseInt(amount) === parseInt(balance), "The receiver should get the amount transfered");
                 test.done();
             });
+        });
+};
+baseTest.test10PercentAdjust = async function (test) {
+    test.expect(2);
+    const connection = baseTest.helpers.getConnection();
+    const contractInstance = baseTest.helpers.getContractInstance();
+    const sender = baseTest.owner;
+    const amount = web3Utils.toWei("110000", "ether");
+    const transactionRequest = {
+        from: sender
+    };
+    const gasEstimate = await connection.eth.estimateGas(transactionRequest);
+    transactionRequest.gas = 20 * gasEstimate;
+    contractInstance.methods.adjust(11000).send(transactionRequest, (error, txHash) => {
+        console.log(error, txHash);
+    })
+        .on('confirmation', function (confirmationNumber, receipt) {
+            console.log('Transaction confirmation:', confirmationNumber, receipt);
+            contractInstance.methods.balanceOf(firstInvestor).call().then(function (firstBalance) {
+                contractInstance.methods.balanceOf(secondInvestor).call().then(function (secondBalance) {
+                    test.ok(parseInt(amount) === parseInt(firstBalance));
+                    test.ok(parseInt(amount) === parseInt(secondBalance));
+                    test.done();
+                });
+            });
+        });
+};
+/**
+ * TODO: Cannot run this test because it is taking too long
+ * causing "nodeunit" closing by time-out.
+ */
+/*
+baseTest.testCyclicAdjust = async function (test) {
+    test.expect(2);
+    const connection = baseTest.helpers.getConnection();
+    const contractInstance = baseTest.helpers.getContractInstance();
+    const sender = baseTest.owner;
+    const amount = web3Utils.toWei("100000", "ether");
+    const transactionRequest = {
+        from: sender
+    };
+    const gasEstimate = await connection.eth.estimateGas(transactionRequest);
+    transactionRequest.gas = 20 * gasEstimate;
+    contractInstance.methods.adjust(11000).send(transactionRequest, (error, txHash) => {
+        console.log(error, txHash);
+    })
+        .on('confirmation', function (confirmationNumber1, receipt1) {
+            console.log('Transaction confirmation:', confirmationNumber1, receipt1);
+            contractInstance.methods.adjust(10000).send(transactionRequest, (error, txHash) => {
+                console.log(error, txHash);
+            })
+                .on('confirmation', function (confirmationNumber2, receipt2) {
+                    console.log('Transaction confirmation 2:', confirmationNumber2, receipt2);
+                    contractInstance.methods.balanceOf(firstInvestor).call().then(function (firstBalance) {
+                        contractInstance.methods.balanceOf(secondInvestor).call().then(function (secondBalance) {
+                            test.ok(parseInt(amount) === parseInt(firstBalance));
+                            test.ok(parseInt(amount) === parseInt(secondBalance));
+                            test.done();
+                        });
+                    });
+                });
         });
 };
 */
