3e775eb41aefdab2e72f7a26c04a7118d6825c78
Vitche Research Team <developer@vitche.com>
2020-09-22 16:59:14 -0400

    1. Simplified calling all tests.

--- test-network.sh
+++ test-network.sh
@@ -1,3 +1,6 @@
+# Clean the previous network state
+rm -rf /tmp/.ethereum/
+
 # TODO: The following module is no more available: db
 # To get more detailed logs: --verbosity 5
 geth --dev --allow-insecure-unlock --rpc --rpcaddr "0.0.0.0" --rpcport "8545" --rpcapi="eth,net,web3,personal,miner" --datadir /tmp/.ethereum/development --ipcpath $HOME/.ethereum/geth.ipc console
---
+++ tests.sh
@@ -0,0 +1,5 @@
+nodeunit tests/accounts.js
+nodeunit tests/dfy-10percent-adjust.js
+nodeunit tests/dfy-10percent-fallout.js
+nodeunit tests/dfy-initial-balance.js
+nodeunit tests/dfy-singular-adjust.js
